8f51a01abc5f5afff825f976b47404d3a0a5a449
Daniel Rodriguez <daniel.rodriguez-vi@mail.escuelaing.edu.co>
2019-03-20 20:39:45 -0500

    Adicion main

---
+++ src/main/java/edu/eci/cvds/samples/services/client/Main.java
@@ -0,0 +1,14 @@
+package edu.eci.cvds.samples.services.client;
+
+import java.sql.SQLException;
+
+import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
+
+
+public class Main {
+	 public static void main(String args[]) throws ExcepcionServiciosAlquiler{
+	        System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquiler().consultarCliente(123456));
+	        System.exit(0);
+	 }        
+}
